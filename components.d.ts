b4b4a070c1c096bb249d645ac1171b671cd797b3
徐友鹏 <361618664@qq.com>
2022-09-18 20:21:16 +0800

    feat(layout): router-view
    主布局完成

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,15 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Menus: typeof import('./src/components/menus/menus.vue')['default']
+    NAvatar: typeof import('naive-ui')['NAvatar']
     NButton: typeof import('naive-ui')['NButton']
     NDropdown: typeof import('naive-ui')['NDropdown']
-    NIcon: typeof import('naive-ui')['NIcon']
+    NGradientText: typeof import('naive-ui')['NGradientText']
     NInput: typeof import('naive-ui')['NInput']
+    NMenu: typeof import('naive-ui')['NMenu']
+    NScrollbar: typeof import('naive-ui')['NScrollbar']
+    PnutAvatar: typeof import('./src/components/pnutAvatar/pnutAvatar.vue')['default']
+    PnutMenu: typeof import('./src/components/pnutMenu/pnutMenu.vue')['default']
   }
 }
--- src/layout/component/main/main.vue
+++ src/layout/component/main/main.vue
@@ -3,7 +3,13 @@
     <div class="w-1/6">
       <menus></menus>
     </div>
-    <div class="w-4/6">2</div>
+    <div class="w-4/6">
+      <div class="w-full h-full bg-amber-700">
+        <n-scrollbar class="h-full w-full">
+          <router-view></router-view>
+        </n-scrollbar>
+      </div>
+    </div>
     <div class="w-1/6">3</div>
   </div>
 </template>

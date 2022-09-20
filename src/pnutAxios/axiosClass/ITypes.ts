bf0e96d907cf8023f21f81748941a8b0e87962e3
徐友鹏 <361618664@qq.com>
2022-09-20 20:22:46 +0800

    feat(axios): axois返回值包装

--- src/pnutAxios/axiosClass/ITypes.ts
+++ src/pnutAxios/axiosClass/ITypes.ts
@@ -4,11 +4,18 @@ import {AxiosRequestConfig, AxiosResponse} from "axios";
 export interface IIntercepter<T = AxiosResponse> {
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig;
   requestInterceptorCatch?: (error: any) => any;
-  responseInterceptor?: (config: T) => T;
+  responseInterceptor?: (response: T) => T;
   responseInterceptorCatch?: (error: any) => any;
 }
 
 // 自定义axios配置类型
 export interface IPnutAxiosConfig<T = AxiosResponse> extends AxiosRequestConfig {
   interceptors?: IIntercepter<T>;
+}
+
+// 通用返回值类型
+export interface IPnutResponse<T = any> {
+  data: T;
+  code: number;
+  message: string;
 }
--- src/pnutAxios/axiosClass/pnutAxiosClass.ts
+++ src/pnutAxios/axiosClass/pnutAxiosClass.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosResponse} from "axios";
 import {AxiosInstance} from "axios"
-import {IPnutAxiosConfig, IIntercepter} from "./ITypes";
+import {IPnutAxiosConfig, IIntercepter, IPnutResponse} from "./ITypes";
 
 export class PnutAxiosClass {
   private axiosInstance: AxiosInstance;
@@ -15,6 +15,8 @@ export class PnutAxiosClass {
 
     // 所有实例公用的拦截器
     this.axiosInstance.interceptors.request.use((config) => {
+        const headers = config.headers as any // 这里由于axios版本的原因   headers的类型中并不包含Authorization  因此需要使用类型断言
+        headers.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjM2NTE1NTQsImV4cCI6MTY2NDI1NjM1NH0.ab62yMnTDQ4zg4w5yXDo9oSvzp2Qii6L2U2XO910W84"
         console.log('Pnut ========> ', "全局拦截器已成功")
         return config;
       },
@@ -41,19 +43,33 @@ export class PnutAxiosClass {
     )
   }
 
-  public GET(config: IPnutAxiosConfig): Promise<any> {
-    return this.commonRequest({...config, method: 'GET'})
+  public GET<T>(config: IPnutAxiosConfig): Promise<any> {
+    return this.commonRequest<T>({...config, method: 'GET'})
   }
 
-  public POST(config: IPnutAxiosConfig): Promise<any> {
-    return this.commonRequest({...config, method: 'POST'})
+  public POST<T>(config: IPnutAxiosConfig): Promise<any> {
+    return this.commonRequest<T>({...config, method: 'POST'})
   }
 
-  // 基于axios的request方法二次封装
-  private commonRequest(config: IPnutAxiosConfig): Promise<AxiosResponse> {
+  public PUT<T>(config: IPnutAxiosConfig): Promise<any> {
+    return this.commonRequest<T>({...config, method: 'PUT'})
+  }
+
+  public DELETE<T>(config: IPnutAxiosConfig): Promise<any> {
+    return this.commonRequest<T>({...config, method: 'DELETE'})
+  }
+
+  /*
+  * 基于axios的request方法二次封装
+  * @param config 请求配置
+  * @returns T 返回值类型
+  * */
+  private commonRequest<T>(config: IPnutAxiosConfig): Promise<IPnutResponse> {
     return new Promise((resolve, reject) => {
-      this.axiosInstance.request(config).then(res => {
-        resolve(res)
+      this.axiosInstance.request<any, AxiosResponse<IPnutResponse>>(config).then(res => {
+        let result: IPnutResponse<T> = {} as IPnutResponse<T>
+        result = {...res.data}
+        resolve(result)
       }).catch(err => {
         reject(err)
       })

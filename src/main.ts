6202c5f2e994d52b423a531ba66cc125b39ea991
徐友鹏 <361618664@qq.com>
2022-09-25 17:17:11 +0800

    feat(Pinia): 引入Pinia
    引入Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import {createApp} from 'vue'
+import {createPinia} from "pinia";
 import './assets/style/index.css'
 import App from './App.vue'
 import registerIcons from "./icon-register";
 
 const app = createApp(App);
+// 引入Pinia
+const pinia = createPinia();
+app.use(pinia)
 // 注册图标
 registerIcons(app);
 app.mount('#app');

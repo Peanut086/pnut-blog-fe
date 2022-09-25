e761d9cae7d721eb2daa7a650b545f758df2e9c1
徐友鹏 <361618664@qq.com>
2022-09-25 22:37:29 +0800

    feat(menus): 抽取props
    抽取props

--- src/components/menus/menus.vue
+++ src/components/menus/menus.vue
@@ -4,7 +4,7 @@
       <pnut-avatar/>
     </div>
     <div class="bg-yellow-500 h-5/6 mt-2 p-2.5">
-      <pnut-menu></pnut-menu>
+      <pnut-menu :menuOptions="menuOptions" title="测试标题"></pnut-menu>
     </div>
   </div>
 </template>
@@ -12,6 +12,30 @@
 <script lang="ts" setup>
 import pnutAvatar from "../pnutAvatar/pnutAvatar.vue"
 import pnutMenu from "../pnutMenu/pnutMenu.vue"
+import {MenuOption, NIcon} from "naive-ui";
+import {RouterLink} from "vue-router";
+import {Component, h} from "vue";
+import {
+  LaptopOutline as WorkIcon,
+  LogOutOutline as HomeIcon
+} from '@vicons/ionicons5'
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, {default: () => h(icon)})
+}
+
+const menuOptions: MenuOption[] = [
+  {
+    label: '工作台',
+    key: 'go-back-home',
+    icon: renderIcon(HomeIcon)
+  },
+  {
+    label: '傻逼',
+    key: 'go-to-work',
+    icon: renderIcon(WorkIcon)
+  }
+]
 </script>
 
 <style scoped>
--- src/components/pnutMenu/pnutMenu.vue
+++ src/components/pnutMenu/pnutMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <n-gradient-text class="text-2xl" type="info">
-      我是傻逼
+      {{ title }}
     </n-gradient-text>
     <n-menu
         :options="menuOptions"
@@ -11,24 +11,19 @@
 </template>
 
 <script lang="ts" setup>
-import {MenuOption, NIcon} from "naive-ui";
-import {RouterLink, useRouter} from "vue-router";
-import {Component, h} from "vue";
-import {
-  LaptopOutline as WorkIcon,
-  LogOutOutline as HomeIcon
-} from '@vicons/ionicons5'
+import {MenuOption} from "naive-ui";
+import {defineProps} from "vue";
 
-const renderIcon = (icon: Component) => {
-  return () => h(NIcon, null, {default: () => h(icon)})
-}
-const menuOptions: MenuOption[] = [
-  {
-    label: '工作台',
-    key: 'work',
-    icon: renderIcon(WorkIcon),
+defineProps<{
+  menuOptions: {
+    type: MenuOption[];
+    default: () => MenuOption[];
+  },
+  title: {
+    type: string;
+    default: () => "";
   }
-]
+}>()
 </script>
 
 <style scoped>
